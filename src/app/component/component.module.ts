accc690b73842c9dbe6c554837b78d079370de77
gangtieshuai <954440430@qq.com>
2018-07-16 15:50:00 +0800

    代码提交
    代码提交

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -3,14 +3,13 @@ import { SharedModule } from '@shared/shared.module';
 import { ListComponent } from './list/list.component';
 import { EnvironmentComponent} from './environment/environment.component';
 import { ListfoldComponent } from './listfold/listfold.component';
-import { MergelistComponent } from './mergelist/mergelist.component';
 import { NzTreeModule } from 'ng-tree-antd';
 import { DndModule } from 'ng2-dnd';
-
+import {MergelistComponent} from './mergelist/mergelist.component'
 const COMPONENT_NOROUNT = [
 
 ];
-
+     
 @NgModule({
   imports: [
     SharedModule,
